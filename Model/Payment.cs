431fe0b92c126590eba242552c3adc3dd8ce799d
Fadil <fadilvtr23@gmail.com>
2025-11-01 09:41:14 +0700

    createPayment.cs

---
+++ Model/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PestControlUts.Model
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int PaymentId { get; set; }
+        public int ClientId { get; set; }
+        public int PackageId { get; set; }
+        public string TransactionCode { get; set; }
+        public double Price  { get; set; }
+        public double Amount { get; set; }
+        public double Total { get; set; }
+    }
+}
