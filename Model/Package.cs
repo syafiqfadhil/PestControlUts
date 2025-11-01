8ee991dcd9d204adab515772cc68fb2b0b111f4e
fajar-islamic <fajarislami@DESKTOP-DLIGIGV>
2025-11-01 09:22:40 +0700

    create package.cs

---
+++ Model/Package.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PestControlUts.Model
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        public int PackageId { get; set; }
+        public string PackageName { get; set; }
+        public string PackageDescription { get; set; }
+        public string PackagePrice { get; set; }
+
+    }
+}
